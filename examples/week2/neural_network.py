2261bf4432c9975603e0cfdf5ff86d691d8c16c1
Mustafa Karakaplan <karakaplanm@users.noreply.github.com>
2025-09-28 15:34:30 +0300

    Create neural_network.py

---
+++ examples/week2/neural_network.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+data = pd.read_csv('heart_disease.csv')
+X = data.drop('target', axis=1)
+y = data['target']
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+     hidden_layer_sizes=(15, 13),
+     random_state=1, max_iter=1000)
+clf.fit(X, y)
+girdi=np.array([34,0,1,118,210,0,1,
+               192,0,0.7,2,0,2]).reshape(1,-1)
+beklenen = clf.predict(girdi)
+print("Beklenen :", beklenen)
