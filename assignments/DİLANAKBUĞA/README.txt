3d24f6eebac075cf9fb6f3e502415f6338b0f256
Dilan AKBUĞA <03200321040@ogr.inonu.edu.tr>
2025-12-02 21:10:29 +0300

    Add files via upload

---
+++ assignments/DİLANAKBUĞA/README.txt
@@ -0,0 +1,4 @@
+This project trains a linear regression model to predict protein solubility.
+Files:
+- protein_solubility.py
+- data_description.txt
---
+++ assignments/DİLANAKBUĞA/protein_solubility.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Dummy dataset
+data = {
+    "hydrophobicity": [0.3, 0.6, 0.1, 0.8, 0.5],
+    "molecular_weight": [10, 25, 8, 30, 20],
+    "charge": [-1, 1, 0, 1, -1],
+    "solubility": [0.9, 0.3, 0.8, 0.2, 0.6],
+}
+df = pd.DataFrame(data)
+
+X = df[["hydrophobicity", "molecular_weight", "charge"]]
+y = df["solubility"]
+
+model = LinearRegression().fit(X, y)
+
+print("Coefficients:", model.coef_)
+print("Predicted solubility:", model.predict([[0.4, 15, 0]])[0])
