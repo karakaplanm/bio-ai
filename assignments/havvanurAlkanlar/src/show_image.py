b5edc4e4e7e5b3eddbbdf06f411dad7fa12ec755
havvanuralkanlar-bit <havvanuralkanlar5@gmail.com>
2025-12-08 14:26:57 +0300

    Delete assignments/havvanurAlkanlar directory

--- assignments/havvanurAlkanlar/src/show_image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-img = cv2.imread("data/petri_example.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-plt.title("Petri Image")
-plt.axis("off")
-plt.show()
